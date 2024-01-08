59d439a4a8a6b87377a2d6f6e5ea695af01e8fcf
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-08 12:23:27 -0300

    Adding React helmet

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <>
+      <Helmet title="SignIn" />
+      <h1>SignIn</h1>
+    </>
   )
 }
