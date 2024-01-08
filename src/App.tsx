f142ca3315e8e9abd6e74fe8884a826d8141666c
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-08 12:15:15 -0300

    Adding layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+// import { Button } from './components/ui/button'
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
